6a5a1adfa3f13ed847d9b27619a9a753e5387f5d
Vagarth <vagarth419@gmail.com>
2025-10-07 21:44:36 +0530

    feat: integrate ToolbarShapes component into the toolbar and enhance Zoom component with zoom in functionality and scale display

--- src/components/toolbar/index.tsx
+++ src/components/toolbar/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import History from '../history'
 import Zoom from '../zoom'
+import ToolbarShapes from '../shapes'
 
 const Toolbar = () => {
   return (
     <div className='fixed bottom-0 w-full grid grid-cols-3 z-50 p-5'>
         <History />
+        <ToolbarShapes />
         <Zoom />
     </div>
   )
