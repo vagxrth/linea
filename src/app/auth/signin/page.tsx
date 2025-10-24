73d426ee4e2cd86d831feb35905332a78b09a62c
Vagarth <vagarth419@gmail.com>
2025-10-25 01:21:42 +0530

    refactor: simplify Google sign-in button in LoginPage and enhance component flexibility

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -31,30 +31,7 @@ export default function LoginPage() {
           </div>
 
           <div className="mt-6 grid grid-cols-2 gap-3">
-            <Google />
-            <Button
-              type="button"
-              variant="outline">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="1em"
-                height="1em"
-                viewBox="0 0 256 256">
-                <path
-                  fill="#f1511b"
-                  d="M121.666 121.666H0V0h121.666z"></path>
-                <path
-                  fill="#80cc28"
-                  d="M256 121.666H134.335V0H256z"></path>
-                <path
-                  fill="#00adef"
-                  d="M121.663 256.002H0V134.336h121.663z"></path>
-                <path
-                  fill="#fbbc09"
-                  d="M256 256.002H134.335V134.336H256z"></path>
-              </svg>
-              <span>Microsoft</span>
-            </Button>
+            <Google className="col-span-2" />
           </div>
 
           <hr className="my-4 border-dashed" />
